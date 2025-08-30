1c9c62a6520cd9e8c3237e2b78e6c8ea42b71788
Gregory <gregory.marshall999@gmail.com>
2025-08-30 11:06:10 +0200

    framework init

---
+++ src/main/java/com/exalt_company/kata_bank_api/KataBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.exalt_company.kata_bank_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataBankApiApplication.class, args);
+	}
+
+}
