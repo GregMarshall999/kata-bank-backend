a5b4437b29f53ee855e16eb94f33d60fe66f0d9f
Greg <gregory.marshall999@gmail.com>
2025-10-16 11:36:25 +0200

    init kata bank with hexagonal architecture

---
+++ src/main/java/com/exalt_company/kata_bank/KataBankApplication.java
@@ -0,0 +1,11 @@
+package com.exalt_company.kata_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataBankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KataBankApplication.class, args);
+    }
+}
