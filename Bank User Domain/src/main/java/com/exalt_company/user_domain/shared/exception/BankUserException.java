5f8e690d5c5cec979269e70ad1874315a96b5e7f
Gregory <gregory.marshall999@gmail.com>
2025-10-18 16:05:54 +0200

    added exception custom message

--- Bank User Domain/src/main/java/com/exalt_company/user_domain/shared/exception/BankUserException.java
+++ Bank User Domain/src/main/java/com/exalt_company/user_domain/shared/exception/BankUserException.java
@@ -3,4 +3,8 @@ package com.exalt_company.user_domain.shared.exception;
 /**
  * Used for Bank User errors
  */
-public class BankUserException extends Exception {}
+public class BankUserException extends Exception {
+    public BankUserException(String message) {
+        super(message);
+    }
+}
