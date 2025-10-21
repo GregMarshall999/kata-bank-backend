ea171d7c306180a0aba23317fde9231fd6afd689
Gregory <gregory.marshall999@gmail.com>
2025-10-21 17:37:16 +0200

    Moved states to correct package

---
+++ Bank User Domain/src/main/java/com/exalt_company/user_domain/api/resource/AdminResponseState.java
@@ -0,0 +1,9 @@
+package com.exalt_company.user_domain.api.resource;
+
+public enum AdminResponseState {
+    CREATED,
+    DELETED,
+    EDITED,
+    FAILED,
+    FOUND, PAGED
+}
--- Bank User Domain/src/main/java/com/exalt_company/user_domain/domain/account/AdminResponse.java
+++
@@ -1,7 +0,0 @@
-package com.exalt_company.user_domain.domain.account;
-
-public enum AdminResponse {
-    CREATED,
-    DELETED,
-    FAILED
-}
