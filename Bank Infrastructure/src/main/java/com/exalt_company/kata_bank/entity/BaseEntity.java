aee6e09048c4b86ce99459249dddafc255d7c524
Greg <gregory.marshall999@gmail.com>
2025-10-22 17:50:21 +0200

    added getters setters to base entity

--- Bank Infrastructure/src/main/java/com/exalt_company/kata_bank/entity/BaseEntity.java
+++ Bank Infrastructure/src/main/java/com/exalt_company/kata_bank/entity/BaseEntity.java
@@ -16,4 +16,20 @@ public class BaseEntity {
 
     @Version
     private int version;
+    
+    public UUID getId() {
+        return id;
+    }
+    
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    
+    public int getVersion() {
+        return version;
+    }
+    
+    public void setVersion(int version) {
+        this.version = version;
+    }
 }
