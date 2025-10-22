b1e28cc6ee1f69000243a921dc1b6ce15252c960
Greg <gregory.marshall999@gmail.com>
2025-10-22 10:31:25 +0200

    account messaging implementation

---
+++ Bank User Domain/src/main/java/com/exalt_company/user_domain/domain/InternalMailer.java
@@ -0,0 +1,62 @@
+package com.exalt_company.user_domain.domain;
+
+import com.exalt_company.user_domain.api.AccountCommunication;
+import com.exalt_company.user_domain.domain.message.CondensedMessage;
+import com.exalt_company.user_domain.domain.message.Message;
+import com.exalt_company.user_domain.domain.message.MessageStatus;
+import com.exalt_company.user_domain.shared.Page;
+import com.exalt_company.user_domain.shared.exception.AccountComunicationException;
+import com.exalt_company.user_domain.shared.exception.MessageException;
+import com.exalt_company.user_domain.spi.Messages;
+
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
+
+public class InternalMailer implements AccountCommunication<File> {
+    private final Messages<File> messages;
+
+    public InternalMailer(Messages<File> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public Message<File> consultMessage(UUID messageId) throws AccountComunicationException {
+        try {
+            return messages.getMessageById(messageId);
+        } catch (MessageException e) {
+            throw new AccountComunicationException("Could not get requested message: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Page<CondensedMessage> listMessages(int page, int size, UUID userMessages)
+            throws AccountComunicationException {
+
+        try {
+            Page<Message<File>> messagePage = messages.pageUserMessages(page, size, userMessages);
+
+            List<CondensedMessage> condensedMessages = messagePage.content().stream()
+                    .map(message -> new CondensedMessage(
+                            message.getId(),
+                            message.getDate(),
+                            message.getContent().substring(0, 10) + "...",
+                            message.getRecipient()
+                    ))
+                    .toList();
+
+            return new Page<>(condensedMessages, messagePage.page(), messagePage.size());
+        } catch (MessageException e) {
+            throw new AccountComunicationException("Failed to list messages: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public MessageStatus sendMessageTo(Message<File> message) throws AccountComunicationException {
+        try {
+            return messages.sendMessageTo(message);
+        } catch (MessageException e) {
+            throw new AccountComunicationException("Could not send message: " + e.getMessage());
+        }
+    }
+}
