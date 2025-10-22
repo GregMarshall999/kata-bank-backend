55c2dfd4f9b1eee5c5acf325ac5775c52822c37c
Greg <gregory.marshall999@gmail.com>
2025-10-22 10:30:30 +0200

    added API exceptions

--- Bank User Domain/src/main/java/com/exalt_company/user_domain/api/AccountCommunication.java
+++ Bank User Domain/src/main/java/com/exalt_company/user_domain/api/AccountCommunication.java
@@ -4,11 +4,12 @@ import com.exalt_company.user_domain.domain.message.CondensedMessage;
 import com.exalt_company.user_domain.domain.message.Message;
 import com.exalt_company.user_domain.domain.message.MessageStatus;
 import com.exalt_company.user_domain.shared.Page;
+import com.exalt_company.user_domain.shared.exception.AccountComunicationException;
 
 import java.util.UUID;
 
 public interface AccountCommunication<F> {
-    Message<F> consultMessage(UUID messageId);
-    Page<CondensedMessage> listMessages(int page, int size, UUID userMessages);
-    MessageStatus sendMessageTo(Message<F> message);
+    Message<F> consultMessage(UUID messageId) throws AccountComunicationException;
+    Page<CondensedMessage> listMessages(int page, int size, UUID userMessages) throws AccountComunicationException;
+    MessageStatus sendMessageTo(Message<F> message) throws AccountComunicationException;
 }
---
+++ Bank User Domain/src/main/java/com/exalt_company/user_domain/shared/exception/AccountComunicationException.java
@@ -0,0 +1,7 @@
+package com.exalt_company.user_domain.shared.exception;
+
+public class AccountComunicationException extends Exception {
+    public AccountComunicationException(String message) {
+        super(message);
+    }
+}
