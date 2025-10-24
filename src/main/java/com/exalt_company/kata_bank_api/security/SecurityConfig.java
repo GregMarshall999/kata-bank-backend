73bdee814e78786d414e988e73bc3dd4634b224d
Gregory <gregory.marshall999@gmail.com>
2025-10-24 17:27:22 +0200

    CORS config and extra endpoint access

--- src/main/java/com/exalt_company/kata_bank_api/security/SecurityConfig.java
+++ src/main/java/com/exalt_company/kata_bank_api/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.exalt_company.kata_bank_api.security;
 
 import com.exalt_company.kata_bank_api.enums.BankRole;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -9,15 +11,21 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
     private final AuthenticationProvider provider;
     private final JwtAuthorizationFilter jwtAuthFilter;
+    private final String frontEndApp;
 
     private static final String[] allowedEndPoints = {
             "/api/auth/**",
@@ -31,8 +39,10 @@ public class SecurityConfig {
 
     private static final String[] clientEndpoints = {
             "/api/audit-account/statement/**",
-            "/api/fund/deposit", "/api/fund/withdraw", "/api/fund/request-overdraw", "/api/fund/cancel-overdraw",
+            "/api/fund", "/api/fund/deposit", "/api/fund/withdraw", "/api/fund/request-overdraw",
+            "/api/fund/cancel-overdraw", "/api/fund/getUserFund/**",
             "/api/saving/open", "/api/saving/close", "/api/saving/deposit", "/api/saving/withdraw",
+            "/api/saving/getUserSaving/**"
     };
 
     private static final String[] adminEndpoints = {
@@ -43,9 +53,13 @@ public class SecurityConfig {
     };
 
     @Autowired
-    public SecurityConfig(AuthenticationProvider provider, JwtAuthorizationFilter jwtAuthFilter) {
+    public SecurityConfig(
+            AuthenticationProvider provider,
+            JwtAuthorizationFilter jwtAuthFilter,
+            @Value("${allowed.origin}") String frontEndApp) {
         this.provider = provider;
         this.jwtAuthFilter = jwtAuthFilter;
+        this.frontEndApp = frontEndApp;
     }
 
     /**
@@ -60,7 +74,18 @@ public class SecurityConfig {
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
+        http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+
+                    config.setAllowedOrigins(List.of(frontEndApp));
+                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                    config.setAllowedHeaders(List.of("*"));
+                    config.setAllowCredentials(true);
+
+                    return config;
+                }))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         requests -> requests.requestMatchers(allowedEndPoints).permitAll()
                         .requestMatchers(clientEndpoints).hasAnyAuthority(BankRole.CLIENT.name(), BankRole.ADMIN.name())
