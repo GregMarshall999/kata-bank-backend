95935ece20bd7eed0ebcb6e7f5dbde22d8203149
Greg <gregory.marshall999@gmail.com>
2025-11-18 11:49:37 +0100

    fixed id generation strategy

--- Bank Infrastructure/src/main/java/com/exalt_company/kata_bank/entity/BaseEntity.java
+++ Bank Infrastructure/src/main/java/com/exalt_company/kata_bank/entity/BaseEntity.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @MappedSuperclass
 public class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Version
