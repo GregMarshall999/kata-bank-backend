82a927859ff291e2b1b29c87fd92b68ae4bfbbb1
Greg <gregory.marshall999@gmail.com>
2025-11-18 11:52:32 +0100

    BankFile name refactored

--- Bank Infrastructure/src/main/java/com/exalt_company/kata_bank/repository/BankFileRepository.java
+++ Bank Infrastructure/src/main/java/com/exalt_company/kata_bank/repository/BankFileRepository.java
@@ -1,8 +1,8 @@
 package com.exalt_company.kata_bank.repository;
 
-import com.exalt_company.kata_bank.entity.BankFile;
+import com.exalt_company.kata_bank.entity.MessageBankFile;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BankFileRepository extends BaseRepository<BankFile> {
+public interface BankFileRepository extends BaseRepository<MessageBankFile> {
 }
