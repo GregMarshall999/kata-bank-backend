6e57bfde969b143e31f21dc28e86e0eb35a71cbd
Greg <gregory.marshall999@gmail.com>
2025-12-16 18:47:16 +0100

    explicite entity scanning

--- Bank_Infrastructure/src/main/java/com/exalt_company/kata_bank/KataBankApplication.java
+++ Bank_Infrastructure/src/main/java/com/exalt_company/kata_bank/KataBankApplication.java
@@ -2,8 +2,12 @@ package com.exalt_company.kata_bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.exalt_company.kata_bank.entity")
+@EnableJpaRepositories(basePackages = "com.exalt_company.kata_bank.repository")
 public class KataBankApplication {
     public static void main(String[] args) {
         SpringApplication.run(KataBankApplication.class, args);
